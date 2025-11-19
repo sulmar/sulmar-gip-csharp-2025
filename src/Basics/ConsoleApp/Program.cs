4ea1d3c007f3cf54c70204955fb4661c745f11e5
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2025-11-19 10:30:05 +0100

    Dodanie aplikacji z obsluga konsoli

---
+++ src/Basics/ConsoleApp/Program.cs
@@ -0,0 +1,9 @@
+﻿
+Console.Write("Podaj imie: ");
+var firstName = Console.ReadLine();
+
+Console.Write("Podaj nazwisko: ");
+var lastName = Console.ReadLine();
+
+string message = firstName + " " + lastName;
+Console.WriteLine(message);
