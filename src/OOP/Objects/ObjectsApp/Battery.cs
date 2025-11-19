0f6592460b6440581808f95a126b84117f7d0dd8
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2025-11-19 14:05:43 +0100

    Dodanie obslugi wyjatkow

--- src/OOP/Objects/ObjectsApp/Battery.cs
+++ src/OOP/Objects/ObjectsApp/Battery.cs
@@ -5,10 +5,33 @@ public class Battery
     public float Capacity;
     public float Level; // 0 = 0% 0.5 = 50% 1 = 100% 
 
+    public const float MaxLevel = 1;
+
+
     // konstruktor
     public Battery()
     {
         Level = 1;
+    }
+
+    public void Charge(float delta)
+    {
+        ArgumentOutOfRangeException.ThrowIfNegativeOrZero(delta);
+
+        //if (delta <= 0)
+        //{
+        //    throw new ArgumentOutOfRangeException("delta");
+        //}
+        
+        if (Level + delta > MaxLevel)
+        {
+            throw new Exception($"Nie mozna naladowac powyzej poziom maks. {MaxLevel:P0}");
+        }
+
+        // Level = Level + delta;
+        Level += delta;
+
+      
     }
 
     private string GetFullName()
--- src/OOP/Objects/ObjectsApp/Program.cs
+++ src/OOP/Objects/ObjectsApp/Program.cs
@@ -5,19 +5,37 @@ Console.WriteLine("Hello, Objects!");
 Battery battery1 = new Battery();
 battery1.Capacity = 1000.5f;
 
-Phone phone1 = new Phone("nokia 3310", battery: battery1 ) { serialNumber = "0123456789" }; // tworzymy nowy obiekt klasy Phone
+Phone phone1 = new Phone("nokia 3310", battery: battery1) { serialNumber = "0123456789" }; // tworzymy nowy obiekt klasy Phone
 phone1.color = "black";
 phone1.cost = 1000;
 
-phone1.Display();
+Console.WriteLine(phone1);
 
 Battery battery2 = new Battery();
 battery2.Level = 0.5f;
 battery2.Capacity = 2000.5f;
 
+// ladowanie baterii
+
+try
+{
+    for (int i = 0; i < 10; i++)
+    {
+        battery2.Charge(-0.2f);
+    }
+}
+catch (Exception e)
+{
+    Console.ForegroundColor = ConsoleColor.Red;
+    Console.WriteLine(e.Message);
+    Console.ResetColor();
+}
+
+
 Phone phone2 = new Phone("Samsung 777", "Samsung", battery2) { serialNumber = "0222456789" };
 phone2.color = "red";
-phone2.Display();
+Console.WriteLine(phone2);
+
 
 
 
