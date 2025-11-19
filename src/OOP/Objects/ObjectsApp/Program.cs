7af7aab83042bbc204d2dd3438f909d4ab12ebc2
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2025-11-19 12:02:01 +0100

    Dodanie przykladu OOP

---
+++ src/OOP/Objects/ObjectsApp/Program.cs
@@ -0,0 +1,22 @@
+﻿Console.WriteLine("Hello, Objects!");
+
+string model = "nokia 3310";
+string manufacture = "Nokia";
+float bateryCapacity = 1000.5f;
+string color = "black";
+string serialNumber = "0123456789";
+
+Console.WriteLine($"model: {model} color: {color} s/n: {serialNumber}");
+
+string model2 = "nokia 7700";
+string manufacture2 = "Nokia";
+float bateryCapacity2 = 2000.5f;
+string color2 = "red";
+string serialNumber2 = "0222456789";
+
+Console.WriteLine($"model: {model2} color: {color} s/n: {serialNumber2}");
+
+
+
+
+
