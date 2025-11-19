9e088783426b7d3ec60d6f00cbd298984bb98c54
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2025-11-19 12:48:33 +0100

    Dodanie kompozycji obiektów

--- src/OOP/Objects/ObjectsApp/Phone.cs
+++ src/OOP/Objects/ObjectsApp/Phone.cs
@@ -6,32 +6,42 @@ using System.Threading.Tasks;
 
 namespace GIP.Warehouse;
 
+public class Battery
+{
+    public float Capacity;
+    public float Level; // 0 = 0% 0.5 = 50% 1 = 100% 
+
+    // konstruktor
+    public Battery()
+    {
+        Level = 1;
+    }
+}
+
 // Klasa
 public class Phone
 {
     // Pola
     public string model;
     public string manufacture;
-    public float batteryCapacity;
-    public float batteryLevel; // 0 = 0% 0.5 = 50% 1 = 100% 
+    public Battery? battery;
     public string color;
     public required string serialNumber;
     public decimal cost;
 
     // constructor
     // sluzy do ustawiania wartosci domyslnych oraz wymaganych parametrow
-    public Phone(string model, string manufacture = "Nokia")
+    public Phone(string model, string manufacture = "Nokia", Battery battery = null)
     {
         this.model = model;
-        this.manufacture = manufacture;
-
-        batteryLevel = 1;
+        this.manufacture = manufacture;       
+        this.battery = battery;
     }
 
     // Metoda
     public void Display()
     {
-        Console.WriteLine($"model: {model} manufacture: {manufacture} color: {color} s/n: {serialNumber} level: {batteryLevel:P2} cost: {cost:C2}");
+        Console.WriteLine($"model: {model} manufacture: {manufacture} color: {color} s/n: {serialNumber} capacity: {battery.Capacity} level: {battery.Level:P2} cost: {cost:C2}");
     }
 
 }
--- src/OOP/Objects/ObjectsApp/Program.cs
+++ src/OOP/Objects/ObjectsApp/Program.cs
@@ -2,16 +2,20 @@
 
 Console.WriteLine("Hello, Objects!");
 
-Phone phone1 = new Phone("nokia 3310") { serialNumber = "0123456789" }; // tworzymy nowy obiekt klasy Phone
-phone1.batteryCapacity = 1000.5f;
+Battery battery1 = new Battery();
+battery1.Capacity = 1000.5f;
+
+Phone phone1 = new Phone("nokia 3310", battery: battery1 ) { serialNumber = "0123456789" }; // tworzymy nowy obiekt klasy Phone
 phone1.color = "black";
 phone1.cost = 1000;
 
 phone1.Display();
 
-Phone phone2 = new Phone("Samsung 777", "Samsung") { serialNumber = "0222456789" };
-phone2.batteryLevel = 0.5f;
-phone2.batteryCapacity = 2000.5f;
+Battery battery2 = new Battery();
+battery2.Level = 0.5f;
+battery2.Capacity = 2000.5f;
+
+Phone phone2 = new Phone("Samsung 777", "Samsung", battery2) { serialNumber = "0222456789" };
 phone2.color = "red";
 phone2.Display();
 
