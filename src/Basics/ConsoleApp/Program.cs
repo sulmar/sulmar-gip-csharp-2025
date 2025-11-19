d4b72f6058f7a167f3d8fd86fe76f0131144b0f3
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2025-11-19 11:15:53 +0100

    Dodanie nullable, const, konwersja

--- src/Basics/ConsoleApp/Program.cs
+++ src/Basics/ConsoleApp/Program.cs
@@ -5,12 +5,59 @@ var firstName = Console.ReadLine();
 Console.Write("Podaj nazwisko: ");
 var lastName = Console.ReadLine();
 
-// zla praktyka
-// string message = firstName + " " + lastName;
+byte age = 30; // 0..255
+Console.Write("Podaj wiek: ");
 
-// dobra praktyka
-string message = $"{firstName} {lastName}"; // Interpolacja 
+// Nullable
+decimal? salary = null;
 
-Console.WriteLine(message);
+if (salary.HasValue)
+{
+    Console.WriteLine($"Salary {salary}");
+}
 
-Console.WriteLine($"{firstName} {lastName}"); // wersja skrocona
+const byte AdultAge = 18;
+
+// Konwersja typów
+age = byte.Parse(Console.ReadLine());
+
+if (byte.TryParse(Console.ReadLine(), out age)) // out - parametr wyjsciowy
+{
+    // zla praktyka
+    // string message = firstName + " " + lastName;
+
+    // dobra praktyka
+    string message = $"{firstName} {lastName}"; // Interpolacja 
+
+    Console.WriteLine(message);
+
+    Console.WriteLine($"imie: {lastName} nazwisko: {firstName} wiek: {age}"); // wersja skrocona
+}
+else
+{
+    Console.WriteLine("Podano nieprawidlowy wiek");
+}
+
+
+if (age >= AdultAge)  
+{
+    Console.WriteLine("Dozwolone");
+}
+else
+{
+    Console.WriteLine("Niedozwolone.");
+}
+
+
+// Rzutowanie typow
+
+// Konwersja jawna (stratna)
+double latitude = 26.01;
+int lat = (int) latitude;
+
+// Konwersja niejawna (bezstratna)
+int longitude = 52;
+double lng = longitude;
+
+
+
