3d091337ecb9efed7aea1aed7e7424b76f09ea11
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2025-11-20 11:27:50 +0100

    Dodanie przykladu przeliczania kursu waluty

---
+++ src/CurrencyCalculatorConsoleApp/NbpCurrencyRateProvider.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Json;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CurrencyCalculatorConsoleApp;
+
+internal class NbpCurrencyRateProvider
+{
+    public decimal GetRate(string currencyCode)
+    {
+        var url = $"https://api.nbp.pl/api/exchangerates/rates/A/{currencyCode}?format=json";
+
+        HttpClient http = new HttpClient();
+
+        NbpResponse data = http.GetFromJsonAsync<NbpResponse>(url).Result;
+
+        return data.Rates[0].Mid;
+    }
+
+
+    public class NbpResponse
+    {
+        public string Table { get; set; }
+        public string Currency { get; set; }
+        public string Code { get; set; }
+        public List<NbpRate> Rates { get; set; }
+    }
+
+    public class NbpRate
+    {
+        public decimal Mid { get; set; }
+    }
+}
---
+++ src/CurrencyCalculatorConsoleApp/Program.cs
@@ -0,0 +1,15 @@
+﻿
+using CurrencyCalculatorConsoleApp;
+
+Console.WriteLine("Hello, World!");
+
+decimal amount = 100;
+string code = "EUR";
+
+NbpCurrencyRateProvider provider = new NbpCurrencyRateProvider();
+
+decimal rate = provider.GetRate(code);
+
+decimal currencyAmount = amount * rate;
+
+Console.WriteLine($"{amount:N2} {code} -> {currencyAmount:C2}");
