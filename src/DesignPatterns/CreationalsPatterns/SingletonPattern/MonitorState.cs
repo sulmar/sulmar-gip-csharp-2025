df9c88146f6399a2100a98fd5192dd3596429f87
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2025-11-21 13:53:06 +0100

    Dodanie problemu

---
+++ src/DesignPatterns/CreationalsPatterns/SingletonPattern/MonitorState.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SingletonPattern;
+
+internal class MonitorState
+{
+    public int Enqueued { get; set; }
+    public int Processed { get; set; }
+    public int Sent { get; set; }
+
+    public override string ToString()
+    {
+        return $"Enqueued: {Enqueued} Processed: {Processed} Sent: {Sent}";
+    }
+
+}
---
+++ src/DesignPatterns/CreationalsPatterns/SingletonPattern/Program.cs
@@ -0,0 +1,22 @@
+﻿
+using SingletonPattern;
+
+Console.WriteLine("Hello, World!");
+
+MonitorState state = new MonitorState();
+state.Enqueued++;
+state.Enqueued++;
+
+Console.WriteLine(state);
+
+state.Enqueued--;
+state.Processed++;
+
+Console.WriteLine(state);
+
+MonitorState state2 = new MonitorState();
+
+state2.Processed--;
+state2.Sent++;
+
+Console.WriteLine(state);
