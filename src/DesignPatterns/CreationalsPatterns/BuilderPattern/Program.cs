89d497a67d1807372763d2b08f91d0ef4e222a59
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2025-11-21 15:30:20 +0100

    Dodanie fabryki

--- src/DesignPatterns/CreationalsPatterns/BuilderPattern/Program.cs
+++ src/DesignPatterns/CreationalsPatterns/BuilderPattern/Program.cs
@@ -1,11 +1,26 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using BuilderPattern;
+using static BuilderPattern.ReportBuilderFactory;
 
 Console.WriteLine("Hello, World!");
 
 
 Report report = new Report { Count = 10, TotalAmount = 1000, Title = "Raport sprzedazy" };
 
-var content = report.Create();
+
+Console.WriteLine("Podaj format: (0) Text  (1) Html ");
+var format = Console.ReadLine();
+
+FormatType formatType = Enum.Parse<FormatType>(format);
+
+ReportBuilderFactory factory = new ReportBuilderFactory();
+var reportBuilder = factory.Create(report, formatType);
+
+// Director
+reportBuilder.AddHeader();
+reportBuilder.AddContent();
+reportBuilder.AddFooter();
+
+var content = reportBuilder.Build();
 
 Console.WriteLine(content);
--- src/DesignPatterns/CreationalsPatterns/BuilderPattern/Report.cs
+++ src/DesignPatterns/CreationalsPatterns/BuilderPattern/Report.cs
@@ -6,50 +6,184 @@ using System.Threading.Tasks;
 
 namespace BuilderPattern;
 
+// Wzorzec Budowniczy 
+// odziela obiekt od jego reprezentacji
+
+// Abstract Builder
+interface IReportBuilder
+{
+    void AddHeader();
+    void AddContent();
+    void AddFooter();
+
+    string Build();
+}
+
+// Concrete Builder A
+class TextReportBuilder : IReportBuilder
+{
+    private readonly Report report;
+
+    private string content = string.Empty;
+
+    public TextReportBuilder(Report report)
+    {
+        this.report = report;
+    }
+
+    public void AddHeader()
+    {
+        content += $"Raport {report.Title} z dn. {DateTime.Now}";
+    }
+
+    public void AddContent()
+    {
+        content += $"Wartosc sprzedazy: {report.TotalAmount:C2}";
+
+        content += $"Ilosc artykulow: {report.Count}";
+    }
+
+    public void AddFooter()
+    {
+        content += "Wygenerowano w aplikacji Builder";
+    }
+
+    // Product
+    public string Build()
+    {
+        return content;
+    }
+
+
+}
+
+// Concrete Builder B
+class HtmlReportBuilder : IReportBuilder
+{
+    private readonly Report report;
+    private string html;
+
+    public HtmlReportBuilder(Report report)
+    {
+        this.report = report;
+    }
+
+    public void AddContent()
+    {
+        html += $"<p>Wartosc sprzedazy: {report.TotalAmount:C2}</p>";
+
+        html += $"Ilosc artykulow: {report.Count}";
+    }
+
+    public void AddFooter()
+    {
+        html += "Wygenerowano w aplikacji Builder";
+    }
+
+    public void AddHeader()
+    {
+        html += $"<h1>Raport {report.Title} z dn. {DateTime.Now}</h1>";
+    }
+
+    // Product
+    public string Build()
+    {
+        return html;
+    }
+}
+
 internal class Report
 {
     public string Title {  get; set; }
-
     public decimal TotalAmount { get; set; }
-
     public int Count { get; set; }
+}
 
-    public string Create()
+
+// Concrete Builder C
+class MarkdownBuilder : IReportBuilder
+{
+    private readonly Report report;
+
+    private StringBuilder builder = new StringBuilder();
+
+    public MarkdownBuilder(Report report)
     {
-        string content = string.Empty;
-
-        // Header
-        content += $"Raport {Title} z dn. {DateTime.Now}";
-
-        // Content
-        content += $"Wartosc sprzedazy: {TotalAmount:C2}";
-
-        content += $"Ilosc artykulow: {Count}";
-
-        // Footer
-        content += "Wygenerowano w aplikacji Builder";
-
-        return content;
-
+        this.report = report;
     }
 
-    public string CreateHtml()
+    public void AddContent()
     {
-        string content = string.Empty;
+        builder.AppendLine($"_Wartosc sprzedazy: {report.TotalAmount:C2}_");
 
-        // Header
-        content += $"<h1>Raport {Title} z dn. {DateTime.Now}</h1>";
+        builder.AppendLine($"Ilosc artykulow: {report.Count}");       
+    }
 
-        // Content
+    public void AddFooter()
+    {
+        builder.AppendLine("Wygenerowano w aplikacji Builder");
+    }
 
-        content += $"<p>Wartosc sprzedazy: {TotalAmount:C2}</p>";
+    public void AddHeader()
+    {
+        builder.AppendLine( $"*Raport {report.Title} z dn. {DateTime.Now}*");
+    }
 
-        content += $"Ilosc artykulow: {Count}";
+    // Product
+    public string Build()
+    {
+        return builder.ToString();
+    }
+}
 
-        // Footer
-        content += "Wygenerowano w aplikacji Builder";
 
-        return content;
+class PdfBuilder 
+{
+    public PdfBuilder(Report report)
+    {
+        
+    }
+    public void AddContent()
+    {
+        throw new NotImplementedException();
+    }
 
+    public void AddFooter()
+    {
+        throw new NotImplementedException();
+    }
+
+    public void AddHeader()
+    {
+        throw new NotImplementedException();
+    }
+
+    public byte[] Build()
+    {
+        throw new NotImplementedException();
+    }
+}
+
+
+class ReportBuilderFactory
+{
+    public IReportBuilder Create(Report report, FormatType format)
+    {
+        switch (format)
+        {
+            case FormatType.Text: return new TextReportBuilder(report);
+            case FormatType.Html: return new HtmlReportBuilder(report);
+            case FormatType.Markdown: return new MarkdownBuilder(report);            
+            default:
+                throw new NotSupportedException();
+        }
+    }
+
+    public enum FormatType
+    {
+        Text,
+        Html,
+        Markdown,
+        Pdf
     }
 }
