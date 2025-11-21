88a5e76ad4183b0b97d163d5133a5cabe4a91d5e
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2025-11-21 14:47:14 +0100

    Problem

---
+++ src/DesignPatterns/CreationalsPatterns/BuilderPattern/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+using BuilderPattern;
+
+Console.WriteLine("Hello, World!");
+
+
+Report report = new Report { Count = 10, TotalAmount = 1000, Title = "Raport sprzedazy" };
+
+var content = report.Create();
+
+Console.WriteLine(content);
---
+++ src/DesignPatterns/CreationalsPatterns/BuilderPattern/Report.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BuilderPattern;
+
+internal class Report
+{
+    public string Title {  get; set; }
+
+    public decimal TotalAmount { get; set; }
+
+    public int Count { get; set; }
+
+    public string Create()
+    {
+        string content = string.Empty;
+
+        // Header
+        content += $"Raport {Title} z dn. {DateTime.Now}";
+
+        // Content
+        content += $"Wartosc sprzedazy: {TotalAmount:C2}";
+
+        content += $"Ilosc artykulow: {Count}";
+
+        // Footer
+        content += "Wygenerowano w aplikacji Builder";
+
+        return content;
+
+    }
+
+    public string CreateHtml()
+    {
+        string content = string.Empty;
+
+        // Header
+        content += $"<h1>Raport {Title} z dn. {DateTime.Now}</h1>";
+
+        // Content
+
+        content += $"<p>Wartosc sprzedazy: {TotalAmount:C2}</p>";
+
+        content += $"Ilosc artykulow: {Count}";
+
+        // Footer
+        content += "Wygenerowano w aplikacji Builder";
+
+        return content;
+
+    }
+}
