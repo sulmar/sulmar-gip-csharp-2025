b117b9d1ed77978fc8e96e3ae2e2d93460b98ab6
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2025-11-25 09:16:45 +0100

    Dodanie MotorolaRadio

---
+++ src/DesignPatterns/StructuralsPatterns/AdapterPattern/MotorolaRadio.cs
@@ -0,0 +1,36 @@
+﻿namespace AdapterPattern;
+
+internal sealed class MotorolaRadio
+{
+    private bool _enabled;
+
+    private byte _selectedChannel;
+
+    public MotorolaRadio()
+    {
+        _enabled = false;
+    }
+
+    public void PowerOn()
+    {
+        _enabled = true;
+    }
+
+    public void SelectChannel(byte channel)
+    {
+        _selectedChannel = channel;
+    }
+
+    public void Send(string message)
+    {
+        if (_enabled)
+        {
+            Console.WriteLine($"<xml><send channel={_selectedChannel}>{message}</xml>");
+        }
+    }
+
+    public void PowerOff()
+    {
+        _enabled = false;
+    }
+}
---
+++ src/DesignPatterns/StructuralsPatterns/AdapterPattern/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+using AdapterPattern;
+
+Console.WriteLine("Hello, World!");
+
+MotorolaRadio radio = new MotorolaRadio();
+
+radio.PowerOn();
+
+radio.SelectChannel(10);
+
+radio.Send("Hello World!");
+
+radio.PowerOff();
