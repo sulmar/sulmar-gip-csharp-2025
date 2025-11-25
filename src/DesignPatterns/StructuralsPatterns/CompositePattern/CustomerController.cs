ee32ad3a39bc30b0b64d6d8d4d776eed3c7cae0e
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2025-11-25 10:37:04 +0100

    Dodanie walidacji klienta

---
+++ src/DesignPatterns/StructuralsPatterns/CompositePattern/CustomerController.cs
@@ -0,0 +1,65 @@
+﻿namespace CompositePattern;
+
+class ActionResult { }
+
+class BadRequestObjectResult : ActionResult 
+{
+    private readonly string message;
+
+    public BadRequestObjectResult(string message)
+    {
+        this.message = message;
+    }
+
+    public override string ToString()
+    {
+        return message;
+    }
+}
+
+class CreatedResult : ActionResult {
+
+    public override string ToString()
+    {
+        return "Created.";
+    }
+
+}
+
+class Customer 
+{
+    public string Nip { get; set; }
+}
+
+interface ICustomerValidator
+{
+    bool IsValid(Customer customer);
+}
+
+class NipCustomerValidator : ICustomerValidator
+{
+    public bool IsValid(Customer customer)
+    {
+        return customer.Nip.Length == 13;
+    }
+}
+
+internal class CustomerController
+{
+    private readonly ICustomerValidator _validator;
+
+    public CustomerController(ICustomerValidator validator)
+    {
+        _validator = validator;
+    }
+
+    public ActionResult Post(Customer customer)
+    {
+        if (!_validator.IsValid(customer))
+        {
+            return new BadRequestObjectResult("Invalid customer data");
+        }
+
+        return new CreatedResult();
+    }
+}
---
+++ src/DesignPatterns/StructuralsPatterns/CompositePattern/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+using CompositePattern;
+
+Console.WriteLine("Hello, World!");
+
+Customer validCustomer = new Customer { Nip = "0123456789123" };
+Customer invalidCustomer = new Customer { Nip = "012345678912" };
+
+CustomerController controller = new CustomerController(new NipCustomerValidator());
+
+var result = controller.Post(validCustomer);
+
+Console.WriteLine(result);
+
