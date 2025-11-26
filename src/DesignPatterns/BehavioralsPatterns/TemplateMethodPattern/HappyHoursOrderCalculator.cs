0a70500eb681f972a49ad1fe0c4387c8e165dee7
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2025-11-26 09:21:27 +0100

    Dodanie kalkulatora promocji Happy Hours

---
+++ src/DesignPatterns/BehavioralsPatterns/TemplateMethodPattern/HappyHoursOrderCalculator.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TemplateMethodPattern;
+
+class Order
+{
+    public DateTime OrderDate { get; set; }
+    public decimal TotalAmount { get; set; }
+}
+
+// Happy Hours - 10% upustu w godzinach od 9:00 - 15:00
+internal class HappyHoursOrderCalculator
+{
+    private readonly int from;
+    private readonly int to;
+    private readonly decimal percentage;
+
+    public HappyHoursOrderCalculator(int from, int to, decimal percentage)
+    {
+        this.from = from;
+        this.to = to;
+        this.percentage = percentage;
+    }
+
+    public decimal CalculateDiscount(Order order)
+    {
+        if (order.OrderDate.Hour >= from && order.OrderDate.Hour <= to)
+        {
+            return order.TotalAmount - order.TotalAmount * percentage;
+        }
+        else
+            return order.TotalAmount;
+    }
+}
---
+++ src/DesignPatterns/BehavioralsPatterns/TemplateMethodPattern/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+using TemplateMethodPattern;
+
+Console.WriteLine("Hello, World!");
+
+Order order = new Order { TotalAmount = 1000, OrderDate = DateTime.Parse("2025-11-26 08:59") };
+
+HappyHoursOrderCalculator calculator = new HappyHoursOrderCalculator(9, 15, 0.1m);
+var result = calculator.CalculateDiscount(order);
+
+Console.WriteLine($"{result:C2}");
