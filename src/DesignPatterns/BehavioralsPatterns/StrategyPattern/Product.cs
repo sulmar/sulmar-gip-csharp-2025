db6d547ced62c456aa852f9b861607e8659b75a1
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2025-11-26 10:50:39 +0100

    Dodanie kalkulatora cen

---
+++ src/DesignPatterns/BehavioralsPatterns/StrategyPattern/Product.cs
@@ -0,0 +1,15 @@
+﻿namespace StrategyPattern;
+
+internal class Product
+{   
+    public string Name { get; set; }
+    public decimal UnitPrice { get; set; }
+}
+
+class ProductCalculator
+{
+    public decimal CalculatePrice(Product product, decimal quantity)
+    {
+        return product.UnitPrice * quantity;
+    }
+}
---
+++ src/DesignPatterns/BehavioralsPatterns/StrategyPattern/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+using StrategyPattern;
+
+Console.WriteLine("Hello, World!");
+
+
+Product product = new Product { Name = "a", UnitPrice = 100 };
+decimal quantity = 3;
+
+ProductCalculator calculator = new ProductCalculator();
+
+var result = calculator.CalculatePrice(product, quantity);
+
+Console.WriteLine($"{product.UnitPrice:C2} x {quantity} = {result:C2}");
