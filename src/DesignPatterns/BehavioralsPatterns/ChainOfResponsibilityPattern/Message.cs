592b2988d691918528a95597904d9469c3444352
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2025-11-26 12:06:19 +0100

    refactor: Przeniesienie do metod prywatnych

--- src/DesignPatterns/BehavioralsPatterns/ChainOfResponsibilityPattern/Message.cs
+++ src/DesignPatterns/BehavioralsPatterns/ChainOfResponsibilityPattern/Message.cs
@@ -1,15 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Text.RegularExpressions;
-using System.Threading.Tasks;
+﻿using System.Text.RegularExpressions;
 
 namespace ChainOfResponsibilityPattern;
 
 internal class Message
 {
     public string From { get; set; }
+    public string To { get; set; }
     public string Subject { get; set; }
     public string Body { get; set; }
 }
@@ -21,25 +17,24 @@ class MessageProcessor
     
     public string Process(Message message)
     {
-        throw new NotImplementedException();
-
         // 1. Walidacja nadawcy (biala lista)
-        if (!whitelist.Contains(message.From))
-        {
-            throw new Exception("Nadawca spoza whitelist");
-        }    
+        ValidateFromWhiteList(message);
 
         // 2. Weryfikacja tematu (czy zawiera nr zamowienia)
-        if (!message.Subject.Contains("Nr zam"))
-        {
-            throw new Exception("Brak numeru zamowienia");
-        }
+        ValidateSubjectOrderNumber(message);
 
-
-        // 3. TODO: weryfikacja adresu e-doreczen (AED)
+        // 3. TODO: weryfikacja formatu adresu e-doreczen (AED)
+        // AE:PL-XXXXX-XXXXX-YYYYY-ZZ        
 
         // 4. Ekstracja NIP z wiadomosci
-        string pattern = "";
+        string nip = ValidateAndExtractNip(message);
+
+        return nip;
+    }
+
+    private static string ValidateAndExtractNip(Message message)
+    {
+        string pattern = @"(\d{10}|\d{3}-\d{3}-\d{2}-\d{2}|\d{3}-\d{2}-\d{2}-\d{3})";
         Regex regex = new Regex(pattern);
         Match match = regex.Match(message.Body);
 
@@ -49,8 +44,22 @@ class MessageProcessor
         }
 
         string nip = match.Value;
-       
-
         return nip;
+    }
+
+    private static void ValidateSubjectOrderNumber(Message message)
+    {
+        if (!message.Subject.Contains("Nr zam"))
+        {
+            throw new Exception("Brak numeru zamowienia");
+        }
+    }
+
+    private void ValidateFromWhiteList(Message message)
+    {
+        if (!whitelist.Contains(message.From))
+        {
+            throw new Exception("Nadawca spoza whitelist");
+        }
     }
 }
--- src/DesignPatterns/BehavioralsPatterns/ChainOfResponsibilityPattern/Program.cs
+++ src/DesignPatterns/BehavioralsPatterns/ChainOfResponsibilityPattern/Program.cs
@@ -1,2 +1,13 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using ChainOfResponsibilityPattern;
+
 Console.WriteLine("Hello, World!");
+
+
+Message message = new Message { From = "john@domain.com", Subject = "Nr zam 1", Body = "Lorem ipsum 953-120-45-91" };
+
+MessageProcessor processor = new MessageProcessor();
+var result = processor.Process(message);
+
+
+Console.WriteLine(result);
